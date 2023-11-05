0bd3395c0597d452048fd0e0082ccb378ba41679
icelake0 <ajiboyeag@gmail.com>
2023-11-05 14:12:36 +0000

    integrate login page with firebase auth

--- app/src/main/java/com/C2479785/beebudget/screens/login/LoginScreen.kt
+++ app/src/main/java/com/C2479785/beebudget/screens/login/LoginScreen.kt
@@ -1,5 +1,6 @@
-package com.C2479785.beebudget.screens
+package com.C2479785.beebudget.screens.login
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,23 +13,41 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.C2479785.beebudget.components.BeeBudgetFullLogo
 import com.C2479785.beebudget.components.InputField
 import com.C2479785.beebudget.navigation.AppScreens
+import com.C2479785.beebudget.screens.register.LoginScreenViewModel
 import com.C2479785.beebudget.ui.theme.PrimaryColor
 
 @Composable
-fun LoginScreen(navController : NavController) {
+fun LoginScreen(
+    navController : NavController,
+    viewModel: LoginScreenViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
+) {
+
+    val context = LocalContext.current
+
     val email = rememberSaveable { mutableStateOf("") }
 
     val password = rememberSaveable { mutableStateOf("") }
+
+    val loginFormIsValid = remember(email.value, password.value) {
+        android.util.Patterns.EMAIL_ADDRESS.matcher(email.value).matches()
+                && password.value.trim().isNotEmpty()
+    }
+
+    val loading by viewModel.loading.observeAsState(initial = false)
 
     Surface( modifier = Modifier
         .fillMaxWidth()
@@ -51,9 +70,16 @@ fun LoginScreen(navController : NavController) {
 
             Button(
                 colors = ButtonDefaults.buttonColors(containerColor = PrimaryColor),
-                onClick = { /*TODO*/ },
                 modifier = Modifier.fillMaxWidth(),
                 content = { Text(text = "Login") },
+                enabled = loginFormIsValid && !loading,
+                onClick = {
+                    viewModel.loginUser(email.value, password.value, {
+                        Toast.makeText(context, "Credentials provided is invalid", Toast.LENGTH_SHORT).show()
+                    }) {
+                        navController.navigate(AppScreens.DashboardScreen.name)
+                    }
+                },
             )
             Text(text = "Don't have an account yet?")
             Text(
